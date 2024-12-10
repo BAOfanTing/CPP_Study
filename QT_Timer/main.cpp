c38b93f2e43df15bec651f9d81b089e1b862e5bf
BAOfanTing <1316149505@qq.com>
2024-12-10 15:42:01 +0800

    [QT-TImer]使用线程优化计时

--- QT_Timer/main.cpp
+++ QT_Timer/main.cpp
@@ -1,6 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QIcon>
+#include <QThread>
+#include "timerthread.h"
+#include <QQuickView>
+#include <QQmlContext>
+#include <QtQml>
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
@@ -11,7 +16,18 @@ int main(int argc, char *argv[])
     //设置窗口图标
     app.setWindowIcon(QIcon("icon.svg"));
 
+    //注册计时线程,并将计时线程移动到工作线程
+    TimerThread timerThread;
+    QThread workerThread;
+    timerThread.moveToThread(&workerThread);
+
+    //启动工作线程
+    workerThread.start();
+
     QQmlApplicationEngine engine;
+
+    qmlRegisterType<TimerThread>("com.timerthread",1,0,"TimerThread");
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(
         &engine,
---
+++ QT_Timer/timerthread.h
@@ -0,0 +1,46 @@
+#ifndef TIMERTHREAD_H
+#define TIMERTHREAD_H
+
+#include <QObject>
+#include <QTimer>
+#include <QThread>
+#include <cmath>
+
+/******************************************************************************
+ *
+ * @file       timerthread.h
+ * @brief      把计时放入线程
+ *
+ * @author     纯真丁一郎
+ * @date       2024/09/18
+ * @Blog       https://www.relxdingyilang.cn/
+ * @history
+ *****************************************************************************/
+
+class TimerThread : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TimerThread(QObject *parent = nullptr);
+
+    //判断运行状态
+    bool isRunning = false;
+
+    QString caculateTime(int totaltime);
+
+signals:
+    void timeUpdated(QString totaltimestr); //发送时间给主界面
+    void sig_isRunning(bool isRunning);//发送状态
+
+public slots:
+    void start();
+    void stop();
+    void pause();
+    void onTimeout();
+
+private:
+    int m_totaltime; //总时间
+    QTimer *timer;
+};
+
+#endif // TIMERTHREAD_H
