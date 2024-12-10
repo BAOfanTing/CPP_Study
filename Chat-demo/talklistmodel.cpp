2814e62546409f1c2610d1c41f9c2d5478b4d734
BAOfanTing <1316149505@qq.com>
2024-12-10 15:45:04 +0800

    [TalkListModel]创建listmodel

---
+++ Chat-demo/talklistmodel.cpp
@@ -0,0 +1,87 @@
+#include "talklistmodel.h"
+
+TalkListModel::TalkListModel()
+{
+
+}
+
+int TalkListModel::rowCount(const QModelIndex &parent) const
+{
+    if(parent.isValid()){
+        return 0;
+    }
+    return talkList.count();
+
+}
+QHash<int, QByteArray> TalkListModel::roleNames() const
+{
+    return QHash<int,QByteArray>{
+        //基本信息
+        {Qt::UserRole+0,"id"},
+        {Qt::UserRole+1,"user"},
+        {Qt::UserRole+2,"sender"},
+        {Qt::UserRole+3,"datetime"},
+        {Qt::UserRole+4,"type"},
+        {Qt::UserRole+5,"status"},
+        //文本
+        {Qt::UserRole+100,"text"}
+    };
+}
+
+QVariant TalkListModel::data(const QModelIndex &index, int role) const
+{
+    if(!index.isValid()){
+        return QVariant();
+    }
+    const int row = index.row();
+    auto item = talkList.at(row);
+
+    switch(role)
+    {
+        default:
+            break;
+        case Qt::UserRole+0:
+            return item->id;
+        case Qt::UserRole+1:
+            return item->user;
+        case Qt::UserRole+2:
+            return item->sender;
+        case Qt::UserRole+3:
+            return QDateTime::fromSecsSinceEpoch(item->datetime).toString("yyyy-MM-dd hh:mm");
+        case Qt::UserRole+4:
+            return item->type;
+        case Qt::UserRole+5:
+            return item->status;
+        case Qt::UserRole+100:
+        {
+            //文本
+            TalkDataText *talk_text = static_cast<TalkDataText*>(item.get());
+            return talk_text->text;
+        }
+    }
+}
+
+void TalkListModel::clearModel()
+{
+    beginResetModel();
+    talkList.clear();
+    endResetModel();
+}
+
+void TalkListModel::appendText(const QString &user, const QString &sender, const QString &text)
+{
+    TalkDataText *talk_data = new TalkDataText;
+    talk_data->id = 0;
+    talk_data->user = user;
+    talk_data->sender = sender;
+    talk_data->datetime = QDateTime::currentSecsSinceEpoch();
+    talk_data->type = TalkData::Text;
+    talk_data->status = TalkData::ParseSuccess;
+    talk_data->text = text;
+
+    //插入模型
+    beginInsertRows(QModelIndex(),talkList.count(),talkList.count());
+    talkList.push_back(QSharedPointer<TalkDataBasic>(talk_data));
+    endInsertRows();
+}
+
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # 自己的学习记录,相关的文档可以访问博客
-[个人博客](https://www.relxdingyilang.cn/)
+[个人博客](https://www.relxdingyilang.cn/),一般项目我都有些对应的文章,文章的章节对应与提交记录,磕自行查看
 
-QT_FaceID:人脸考勤系统
+麻了,之前瞎提交把仓库搞没了,还好有复制,家人们一定要先拉在提交
+
+QT_FaceID:人脸考勤系统  [文章](https://www.relxdingyilang.cn/index.php/2024/03/13/qt-%e5%9f%ba%e4%ba%8eqt%e7%9a%84%e4%ba%ba%e8%84%b8%e8%80%83%e5%8b%a4%e7%b3%bb%e7%bb%9f/)
 
 QT-Timer:基于QML的计时器
+
+listModelDemo :自己测试优化gridview和listmodel
+
+QMLsink-Study:学习如何使用qml直接在rk3588上播放RTP流视频
+
+Chat-demo : 仿微信聊天界面搭建
